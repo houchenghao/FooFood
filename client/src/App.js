2d181cf01a4bba1deb4c622757aa8799faf8e9ab
Chenghao Hou <houchenghao@hotmail.com>
2023-05-04 21:48:05 +1000

    client package.json fix

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+
+function App() {
+  return (
+    <div >
+
+    </div>
+  );
+}
+
+export default App;
