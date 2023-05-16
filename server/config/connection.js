89f371798f4fc0e68f5626b0c7350d79149298ac
Chenghao Hou <houchenghao@hotmail.com>
2023-05-16 20:26:28 +1000

    update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const db = require('./config/connection');
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/foofood', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
